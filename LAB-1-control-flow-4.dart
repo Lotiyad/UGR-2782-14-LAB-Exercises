ee7b30f34dab70e184cae48d68ff30a21b8c1608
Loti Yadeta <loti50435@gmail.com>
2024-03-08 04:27:34 -0900

    control

---
+++ LAB-1-control-flow-4.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+void main() {
+  
+  print("Enter the first number:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the second number:");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the operation (+, -, *, /):");
+  String operation = stdin.readLineSync()!;
+
+  double result;
+
+ 
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 != 0) {
+        result = num1 / num2;
+      } else {
+        print("Error: Division by zero is not allowed.");
+        return;
+      }
+      break;
+    default:
+      print("Error: Invalid operation.");
+      return;
+  }
+
+ 
+  print("Result: $result");
+}
