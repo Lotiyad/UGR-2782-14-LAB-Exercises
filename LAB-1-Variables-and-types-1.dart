9a919a06271aa79b6180e0e60f1e2b935439f33c
Loti Yadeta <loti50435@gmail.com>
2024-03-08 04:41:39 -0900

    variables

---
+++ LAB-1-Variables-and-types-1.dart
@@ -0,0 +1,11 @@
+void main() {
+  
+  String name = "John";
+  int age = 30;
+  String favoriteColor = "blue";
+
+  
+  print("My name is $name.");
+  print("I am $age years old.");
+  print("My favorite color is $favoriteColor.");
+}
