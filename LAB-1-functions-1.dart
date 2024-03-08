c0f2ea9026e5e7e83ce54b399340ed333b451e52
Loti Yadeta <loti50435@gmail.com>
2024-03-08 04:35:19 -0900

    function

---
+++ LAB-1-functions-1.dart
@@ -0,0 +1,16 @@
+
+double sum(double num1, double num2) {
+  return num1 + num2;
+}
+
+void main() {
+
+  double number1 = 10;
+  double number2 = 20;
+
+ 
+  double result = sum(number1, number2);
+
+
+  print("The sum of $number1 and $number2 is $result");
+}
